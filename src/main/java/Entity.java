8d510607eba3698c1cc9f7123acc45e36bed9b20
rmhanselman <rmhanselman@gmail.com>
2018-12-31 13:55:38 -0700

    Updated Entity.java
    Added getter and setter methods for data access

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -4,11 +4,11 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class Entity extends ListenerAdapter {
-    public int hp;
-    public int mp;
-    public int atk;
-    public int def;
-    public int lvl;
+    private int hp;
+    private int mp;
+    private int atk;
+    private int def;
+    private int lvl;
 
     public Entity(int health, int mana, int attack, int defense, int level){
         this.hp =  health;
@@ -20,13 +20,52 @@ public class Entity extends ListenerAdapter {
     public int status( int status){
         if(hp <= 0){
             status = -1;
+            System.out.println("Insert name has been killed.");
         } else{
             status = 1;
         }
-        if(status == -1){
-            System.out.println("Insert name has been killed.");
-        }
         return status;
     }
-
+    
+    //Getter Class Methods
+    public int getHP(){
+        return hp;   
+    }
+    
+    public int getMP(){
+        return mp;
+    }
+    
+    public int getATK(){
+        return atk;   
+    }
+    
+    public int getDEF(){
+        return def;
+    }
+    
+    public int getLVL(){
+        return lvl;
+    }
+    
+    //Setter Class Methods
+    public void setHP(int n){
+        hp = n;
+    }
+    
+    public void setMP(int n){
+        mp = n;
+    }
+    
+    public void setATK(int n){
+        atk = n;
+    }
+    
+    public void setDEF(int n){
+        def = n;
+    }
+    
+    public void setLVL(int n){
+        lvl = n;   
+    }
 }
