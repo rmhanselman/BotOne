d3c7c7c8a84c11bc4cdc5704735048d075e69feb
NicoNicoRee <45520985+NicoNicoRee@users.noreply.github.com>
2018-12-31 13:16:53 -0600

    Add files via upload

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,5 +1,7 @@
+import events.Creature;
+import events.Entity;
+import events.Player;
 import events.HelloEvent;
-import events.Introduction;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
@@ -10,6 +12,9 @@ public class Bot {
         JDA jda = new JDABuilder("NTI4Njk2NzcxNDEzNTQwODg2.DwmC5A.qB-WjOQG19ChlCEGExNCS1_704Y").build();
 
         jda.addEventListener(new HelloEvent());
+
+        jda.addEventListener(new Creature(500,5,5,1,5));
+        jda.addEventListener(new Player(1,10,10,10,10));
 
     }
 }
